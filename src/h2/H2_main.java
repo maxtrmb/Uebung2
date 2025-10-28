aaaad295344154bb7ce08f1283c837dfe4c66f32
Max <mvontrue@smail.uni-koeln.de>
2025-10-28 15:36:06 +0100

    Upload H2

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -3,6 +3,33 @@ package h2;
 public class H2_main {
 
 	public static void main(String[] args) {
+		int i= 345, j=453, k=982, min, max;
+		
+		if (i<j && i<k) {
+			
+			min=i;
+			
+		} else if (j<i && j<k) {
+			
+			min=j;
+			
+		} 	else min=k;
+		
+		if (i > j && i > k) {
+			
+            max = i;
+            
+        } else if (j > i && j > k) {
+        	
+            max = j;
+            
+        } 	else {
+        	
+            max = k;
+        } 
+		System.out.println("Minimum=" + min);
+		System.out.println("Maximum=" + max);
+		
 		// TODO Auto-generated method stub
 
 	}
