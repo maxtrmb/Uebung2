dcb855055645f6e9bebbdc2eacc0e7d6a1cd2165
Max <mvontrue@smail.uni-koeln.de>
2025-10-28 11:27:36 +0100

    4.Upload

---
+++ src/p3/P3_main.java
@@ -0,0 +1,27 @@
+package p3;
+
+public class P3_main {
+
+	public static void main(String[] args) {
+		int i = 10, j = 100, k = 1000; 
+		if (i<j) {
+			
+			System.out.println("A");
+			
+		}
+			
+		if (i<j) {
+			
+			System.out.println("B");
+	
+		}
+		
+		if (j<k) {
+			
+			System.out.println("C");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
