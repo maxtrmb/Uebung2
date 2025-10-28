40542257086e2623a9fc38cae3b0ee21a3987363
Max <mvontrue@smail.uni-koeln.de>
2025-10-28 11:02:28 +0100

    3.Upload

---
+++ src/p2/P2_main.java
@@ -0,0 +1,18 @@
+package p2;
+
+public class P2_main {
+
+	public static void main(String[] args) {
+		int i = 15;
+		boolean b = false;
+		if (i>10) {
+			
+			b=true;
+		}
+		System.out.println(b);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
