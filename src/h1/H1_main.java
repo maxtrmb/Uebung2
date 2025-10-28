f12ccd4192904fa025f55716f5f75b518db8cbd1
Max <mvontrue@smail.uni-koeln.de>
2025-10-28 15:13:00 +0100

    Upload H1

---
+++ src/h1/H1_main.java
@@ -0,0 +1,21 @@
+package h1;
+
+public class H1_main {
+
+	public static void main(String[] args) {
+		
+		int z = 73;
+		
+		if (z>10) {
+			
+		z = z-10;
+		
+		}
+		
+		System.out.println(z);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
